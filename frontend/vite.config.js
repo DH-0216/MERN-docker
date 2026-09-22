98abf879685ed68df3cc16c22c01a5653174f285
Dulaj Hashmika <dulajhashmika075@gmail.com>
2026-09-22 17:04:18 +0530

    feat: update backend and frontend images, and configure environment variables for improved flexibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,27 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
-  server: {
-    proxy: { "/api": { target: "http://localhost:5000", changeOrigin: true } },
-  },
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      babel({
+        presets: [reactCompilerPreset()],
+      }),
+    ],
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_PROXY_TARGET || "http://localhost:5000",
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
